86302e99a3ee9d75ce702692cb6addddcb9eba46
Marsa Emreef <marsa.emreef@gmail.com>
2022-09-07 18:34:38 +0400

    .initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,53 @@
 import React from 'react';
+// Import Swiper styles
+import { Navigation, Pagination,Lazy} from 'swiper';
+import 'swiper/css';
+import "swiper/css/lazy";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import {Swiper, SwiperSlide} from "swiper/react";
+import * as images from "./images/images";
+
+
+const imageDataSource:string[] = [images.menu_0,images.menu_1,images.menu_2,images.menu_3,images.menu_4,images.menu_5,images.menu_6,images.menu_7,images.menu_8,images.menu_9,
+    images.menu_10,images.menu_11,images.menu_12,images.menu_13,images.menu_14,images.menu_15,images.menu_16,images.menu_17,images.menu_18,images.menu_19,
+    images.menu_20,images.menu_21,images.menu_22,images.menu_23,images.menu_24,images.menu_25,images.menu_26,images.menu_27,images.menu_28,images.menu_29,
+    images.menu_30
+]
 function App() {
-  return (<div>Helo World</div>);
+    const bgWidth = 907;
+    const bgHeight = 1280;
+    const viewportWidth = document.body.offsetWidth;
+    const viewportHeight = document.body.offsetHeight;
+
+    let imageHeight = viewportHeight;
+    let imageWidth = (viewportHeight / bgHeight) * bgWidth
+    if(imageWidth > viewportWidth){
+        imageHeight = viewportWidth / imageWidth * imageHeight;
+        imageWidth = viewportWidth;
+    }
+    let topMargin = 0;
+    if(viewportHeight > imageHeight){
+        topMargin = (viewportHeight - imageHeight) / 2;
+    }
+    return (<div style={{background:'black',paddingTop:topMargin,height:'100%'}}><Swiper
+        slidesPerView={"auto"}
+        spaceBetween={20}
+        centeredSlides={true}
+        pagination={{
+            clickable: true,
+        }}
+        modules={[Lazy,Pagination,Navigation]}
+        lazy={{loadPrevNext:true,loadPrevNextAmount:3}}
+    >
+        {imageDataSource.map(value => {
+            return <SwiperSlide key={value} style={{background:'black',width:imageWidth,height:imageHeight}} >
+                    <img  data-src={value} alt={'Menu Rempah'} style={{width:imageWidth,height:imageHeight,borderRadius:20}} width={imageWidth} height={imageHeight} className="swiper-lazy"/>
+                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"/>
+                    <div style={{width:imageWidth,height:imageHeight,top:0,position:'absolute',boxShadow:'0 0 2px 3px rgba(0,0,0,1) inset,0 0 30px 3px rgba(0,0,0,0.3) inset',zIndex:1,borderRadius:20}}/>
+            </SwiperSlide>
+        })}
+    </Swiper></div>);
 }
 
 export default App;
---
+++ src/images/images.tsx
@@ -0,0 +1,36 @@
+import menu_0 from "./Menu-00-01-Front.jpg";
+import menu_1 from "./Menu-01-Seafood Jimbaran.jpg";
+import menu_2 from "./Menu-02-Seafood Padang.jpg";
+import menu_3 from "./Menu-03-Ayam Taliwang.jpg";
+import menu_4 from "./Menu-04-Gado-Gado.jpg";
+import menu_5 from "./Menu-05-Siomay.jpg";
+import menu_6 from "./Menu-06-Pempek.jpg";
+import menu_7 from "./Menu-07-Gorengan.jpg";
+import menu_8 from "./Menu-08-Tahu Gejrot.jpg";
+import menu_9 from "./Menu-09-Internet.jpg";
+import menu_10 from "./Menu-10-Sop Buntut.jpg";
+import menu_11 from "./Menu-11-Ayam Kalasan.jpg";
+import menu_12 from "./Menu-12-Mie Ayam Jamur.jpg";
+import menu_13 from "./Menu-13-Nasi Goreng.jpg";
+import menu_14 from "./Menu-14-Mie Dukduk.jpg";
+import menu_15 from "./Menu-15-Sate Ayam.jpg";
+import menu_16 from "./Menu-16-1-Sate Kambing.jpg";
+import menu_17 from "./Menu-16-2-Nasi Padang.jpg";
+import menu_18 from "./Menu-17-Terang Bulan.jpg";
+import menu_19 from "./Menu-18-1-Pisang Goreng Karamel.jpg";
+import menu_20 from "./Menu-18-2-Drinks Preambule.jpg";
+import menu_21 from "./Menu-19-Kopi Luwak.jpg";
+import menu_22 from "./Menu-20-Bir Pletok.jpg";
+import menu_23 from "./Menu-21-Es Teler.jpg";
+import menu_24 from "./Menu-22-Kelapa Muda.jpg";
+import menu_25 from "./Menu-23-Kelapa Jeruk.jpg";
+import menu_26 from "./Menu-24-Teh Tubruk.jpg";
+import menu_27 from "./Menu-25-Kopi Tubruk.jpg";
+import menu_28 from "./Menu-26-Back.jpg";
+import menu_29 from "./Menu-26-Other Drinks.jpg";
+import menu_30 from "./menu-27-Back.jpg";
+export {
+    menu_0,menu_1,menu_2,menu_3,menu_4,menu_5,menu_6,menu_7,menu_8,menu_9,menu_10,menu_11,menu_12,menu_13,
+    menu_14,menu_15,menu_16,menu_17,menu_18,menu_19,menu_20,menu_21,menu_22,menu_23,menu_24,menu_25,menu_26,menu_27,
+    menu_28,menu_29,menu_30,
+}
